8c56e2fafa6a417ff554d50262e1455772de355e
GabrielaGiraldo <giraldogabriela861@gmail.com>
2023-02-12 16:01:09 -0500

    [UPDATE]

---
+++ actividad/cambiar.py
@@ -0,0 +1,15 @@
+def cambiar(
+    diccionario:dict
+) -> dict:
+     x = str(input("Desea modificar un usuario? (si/no) "))
+     x = x.lower()
+     if x == "si":
+         ant = str(input("Nombre de usuario por cambiar:"))
+         ant = ant.lower()
+         nom = str(input("Nombre de usuario: "))
+         nom = nom.lower()
+         diccionario.pop(ant)
+         diccionario.update(nom)
+     else:
+         print("No se desea añadir ningun usuario")
+print("-"*40)
