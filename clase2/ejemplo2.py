e60164fd5ae9aef7303f43566b0210d059f0176a
GabrielaGiraldo <giraldogabriela861@gmail.com>
2023-02-13 20:06:03 -0500

    [UPDATE]

--- clase2/ejemplo2.py
+++ clase2/ejemplo2.py
@@ -1,15 +1,3 @@
-#def funcion_ejemplo(a,b,c=5):
-    # En los parametros de la función a "c" se le asigna un valor "fijo" o "predeterminado" (pre)
-    # Los parametros pre no pueden estar ubicados al inicio [def blabla(c=6,a,b):] <- eso estaría mal aplicado
-    #lógica
-    #d = int(input("Ingrese un numero:"))
-    # si se pide una variable dentro de la misma función no se tiene porque pasar por parametros.
-    #suma = a + b + d
-    #return
-    #return suma
-#print(funcion_ejemplo(2,3))
-
-
 lista_estudiante = ["David","Omar","Isaac"]
 lista_profe = ["Felipe"]
 def agregar(
